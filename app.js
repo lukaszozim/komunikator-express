86d8e834cf015792a117131ed0cc7774902f1e13
lukaszozim <lukasz090@poczta.onet.pl>
2020-11-02 20:44:47 +0100

    usuniecie dotenv

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config({path:'.env'});
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,11 @@
 const Sequelize = require("sequelize");
 
-//połączenie do bazy
 config = {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    database: 'komunikator',
+    username: 'PHP',
+    password: 'mysql',
+    host: 'localhost',
+    dialect: 'mysql'
   };
 
   module.exports = config;
