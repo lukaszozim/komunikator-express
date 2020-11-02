92809190bb96c36b6ebe57bb087aafa2705d2731
lukaszozim <lukasz090@poczta.onet.pl>
2020-11-02 20:01:37 +0100

    zmienne srodowiskowe

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,11 +2,11 @@ const Sequelize = require("sequelize");
 
 //połączenie do bazy
 config = {
-    database: "komunikator",
-    username: "PHP",
-    password: "mysql",
-    host: "127.0.0.1",
-    dialect:'mysql'
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   };
 
   module.exports = config;
