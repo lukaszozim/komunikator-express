af7fdca17d419775efa9f296a01254494821cf33
lukaszozim <lukasz090@poczta.onet.pl>
2020-11-02 21:25:48 +0100

    dotenv

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
 config = {
-    database: 'komunikator',
-    username: 'PHP',
-    password: 'mysql',
-    host: 'localhost',
-    dialect: 'mysql'
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   };
 
   module.exports = config;
